671a5ab82b525f6044abd47b856d76dea72c32c9
a <nightingazer@outlook.com>
2023-01-20 13:40:43 +0100

    optimize random number generation

--- Walnut/src/Walnut/Random.h
+++ Walnut/src/Walnut/Random.h
@@ -45,7 +45,7 @@ namespace Walnut {
 		}
 	private:
 		static thread_local std::mt19937 s_RandomEngine;
-		static thread_local std::uniform_int_distribution<std::mt19937::result_type> s_Distribution;
+		static std::uniform_int_distribution<std::mt19937::result_type> s_Distribution;
 	};
 
 }
